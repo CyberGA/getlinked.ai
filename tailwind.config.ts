81c6acebc3b43ab3d5ada80df49436d5d28837ec
CyberGA <etomu.joshua@gmail.com>
2023-09-19 16:28:04 +0100

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,17 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "c-button-gradient": "linear-gradient(270deg, #903AFF 0%, #D434FE 56.42%, #FF26B9 99.99%, #FE34B9 100%)",
+        "c-text-gradient": "linear-gradient(270deg, #903AFF 0%, #D434FE 56.42%, #FF26B9 99.99%, #FE34B9 100%)"
       },
+      colors: {
+        "c-bg": "#150E28",
+        "c-dark-bg": "#100B20",
+        "c-pink": "#D434FE",
+        "c-dark-pink": "#FF26B9",
+        "c-purple": "#903AFF",
+        "c-green": "#2DE100",
+      }
     },
   },
   plugins: [],
